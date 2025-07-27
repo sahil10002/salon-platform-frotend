f133511cf9f65c42126bb8a3ce70313bb5d31e61
Sahil Malhotra <sahilpatti44@gmail.com>
2025-07-28 01:29:44 +0530

    Update Dashboard.js

--- src/pages/Partner/Dashboard.js
+++ src/pages/Partner/Dashboard.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function PartnerDashboard() {
+  return (
+    <div>
+      <h2>Partner Dashboard</h2>
+      <p>Welcome, partner! Here you can manage your services.</p>
+    </div>
+  );
+}
+
+export default PartnerDashboard;
