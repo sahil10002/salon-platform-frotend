7aedd8f509520d10dc34ddd5a296e15947cec937
Sahil Malhotra <sahilpatti44@gmail.com>
2025-07-28 01:32:49 +0530

    Create Dashboard.js

---
+++ src/pages/Customers/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CustomerDashboard() {
+  return (
+    <div>
+      <h2>Customer Dashboard</h2>
+      <p>Explore and book services here.</p>
+    </div>
+  );
+}
+
+export default CustomerDashboard;
