828ab604357e68f69a7ca36c6e9be1c09626b0c8
Sahil Malhotra <sahilpatti44@gmail.com>
2025-07-28 00:17:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-const API_BASE_URL = "https://salon-backend-8u5u.onrender.com"; // ← your backend URL
-
+const API_BASE_URL = "https://salon-backend-8u5u.onrender.com";
 export default API_BASE_URL;
