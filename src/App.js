d857fcc907de648343b3729ad9d5aea47260c3f3
Sahil Malhotra <sahilpatti44@gmail.com>
2025-07-28 07:51:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import PartnerDashboard from "./pages/Partner/Dashboard";
-import CustomerDashboard from "./pages/Customer/Dashboard";
-import AdminDashboard from "./pages/Admin/Dashboard";
+import PartnerDashboard from "./pages/PartnerDashboard";
+import CustomerDashboard from "./pages/CustomerDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/login">Login</Link> |{" "}
-        <Link to="/register">Register</Link> |{" "}
-        <Link to="/partner">Partner Dashboard</Link> |{" "}
-        <Link to="/customer">Customer Dashboard</Link> |{" "}
-        <Link to="/admin">Admin Dashboard</Link>
-      </nav>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/partner" element={<PartnerDashboard />} />
         <Route path="/customer" element={<CustomerDashboard />} />
